b276a6f464e7b983a32ef798a421dd39195a7d25
Andrew Yablonskiy <dron2310@gmail.com>
2019-11-15 22:10:03 +0300

    Dialer improvements

--- arduino-bluetooth-dialer/arduino-bluetooth-dialer.ino
+++ arduino-bluetooth-dialer/arduino-bluetooth-dialer.ino
@@ -1,19 +1,24 @@
 // пин ввода цифры подключает
-// подключается черзе внешний pullup резистор
-#define PIN_DIALER_NUMBERS 2 
+// подключается через внешний pulldown резистор
+#define PIN_DIALER_NUMBERS 2
 
 // пин "замка" ввода цифры
-// подключается черзе внешний pullup резистор
-#define PIN_DIALER_LOCKER A1 
+// подключается через внешний pulldown резистор
+#define PIN_DIALER_LOCKER A1
 
 #define DEBOUNCER_DIALER_MILLISECONDS 80
-#define DEBOUNCER_DIALER_LOCKER_MILLISECONDS 40
+#define DEBOUNCER_DIALER_LOCKER_MILLISECONDS 80
 
-volatile unsigned long debouncerDilerTimer = 0;
-volatile unsigned char dialerIntrruptCounter = 0; // счетчик вода цифр
+// таймеры компенсации дребезга
+volatile unsigned long debouncerDialerTimer = 0;
+unsigned long debouncerDialerLockerTimer = 0;
 
-unsigned long debouncerDilerLockerTimer = 0;
-bool isDialerLockedPreview = false;
+// счетчик размыканий контактов набирателя
+volatile unsigned char dialerIntrruptCounter = 0; // счетчик вода цифр в интерапторе
+byte dialerCounter = 0;
+
+bool isDialingInProgress = false;
+
 
 void setup() {
   Serial.begin(9600);
@@ -23,14 +28,14 @@ void setup() {
 }
 
 void onDialerIntrrupt() {
-  bool debouncerDialerCounterLocked = millis() - debouncerDilerTimer < DEBOUNCER_DIALER_MILLISECONDS;
-  
- // компенсация дребезга ввода цифр
-  if (!debouncerDialerCounterLocked){
-	debouncerDilerTimer = millis();
-	
-    dialerIntrruptCounter++;  
-  } 
+  bool debouncerDialerCounterLocked = millis() - debouncerDialerTimer < DEBOUNCER_DIALER_MILLISECONDS;
+
+  // компенсация дребезга ввода цифр
+  if (!debouncerDialerCounterLocked) {
+    debouncerDialerTimer = millis();
+
+    dialerIntrruptCounter++;
+  }
 }
 
 void loop() {
@@ -38,52 +43,62 @@ void loop() {
   dialerLoop();
 }
 
-void dialerLoop(){
-	bool debouncerDialerLockerLocked = millis() - debouncerDilerLockerTimer < DEBOUNCER_DIALER_LOCKER_MILLISECONDS;
-	
-	// компенсация дребезга замка ввода
-	if (!debouncerDialerLockerLocked){
-		debouncerDilerLockerTimer = millis();
-		
-		bool isDialerLocked = digitalRead(PIN_DIALER_LOCKER);
-		//  Serial.print("isDialerLocked: ");
-		//  Serial.println(isDialerLocked);
-		
-		if (isDialerLocked != isDialerLockedPreview){
-			isDialerLockedPreview = isDialerLocked;
-			onDilerLockerStateChanged(isDialerLocked)
-		} 
-	}
+void dialerLoop() {
+  bool debouncerDialerLockerLocked = millis() - debouncerDialerLockerTimer < DEBOUNCER_DIALER_LOCKER_MILLISECONDS;
+  
+  // компенсация дребезга замка ввода
+  if (!debouncerDialerLockerLocked) {
+        debouncerDialerLockerTimer = millis();
+
+        bool isDialingInProgressUpdated = digitalRead(PIN_DIALER_LOCKER);
+        byte dialerCounterUpdated = getCurrentDialerIntrruptCounter();
+
+        // значение счетчика было увеличено
+        bool isDialerCounterIncreased = dialerCounter < dialerCounterUpdated;
+        
+        // замок перешел с состояния "разблокированный" в "заблокированный"        
+        bool isDialingFinished = isDialingInProgress && !isDialingInProgressUpdated;
+        
+        if (isDialerCounterIncreased || (dialerCounterUpdated > 0 && isDialingFinished)){           
+            onDialerInput(!isDialingFinished, dialerCounterUpdated);       
+        }
+
+        // ввод закончен
+        if (isDialingFinished){           
+            resetCurrentDialerIntrruptCounter();
+        }
+
+        isDialingInProgress = isDialingInProgressUpdated;
+        dialerCounter = dialerCounterUpdated;
+   }
 }
 
-void onDilerLockerStateChanged(bool isLocked){
-	char dialerCounter = getCurrentDialerIntrruptCounter();
-	char dialerValue = dialerCounter >= 10 ? 0 : dialerCounter;
-	printValue(isLocked, dialerValue);
-
-	if(isLocked){
-		// ввод закончен
-		resetCurrentDialerIntrruptCounter();
-	} else {
-		// ввод начат  
-	}
+void onDialerInput(bool isInProgress, byte dialerCounter){    
+    byte dialerValue = dialerCounter >= 10 ? 0 : dialerCounter;
+    printValue(!isInProgress, dialerValue);
+    
+//    if (isInputInProgress) {
+//        // ввод закончен           
+//    } else {
+//        // ввод начат
+//    }
 }
 
-char getCurrentDialerIntrruptCounter() {
-   noInterrupts(); 
-   char dialerCounter = dialerIntrruptCounter;
-   interrupts();
-   return dialerCounter;
+byte getCurrentDialerIntrruptCounter() {
+    noInterrupts();
+    char dialerCounter = dialerIntrruptCounter;
+    interrupts();
+    return dialerCounter;
 }
 
 void resetCurrentDialerIntrruptCounter() {
-   noInterrupts(); 
-   dialerIntrruptCounter = 0;
-   interrupts();
+    noInterrupts();
+    dialerIntrruptCounter = 0;
+    interrupts();
 }
 
-void printValue(bool isFinal, unsigned char value){
-	Serial.print("input value: ");
-	Serial.println(value);
+void printValue(bool isFinal, byte value) {
+    if(isFinal) Serial.print("[F]");
+    Serial.print("input value: ");
+    Serial.println(value);
 }
-
