d37f834b647be41b1f8a47142d2e5aa78316f0c2
Andrew Yablonskiy <dron2310@gmail.com>
2019-12-02 01:20:15 +0300

    AltSoftSerial base implementation

--- BK8000L_SPP/BK8000L_SPP.ino
+++ BK8000L_SPP/BK8000L_SPP.ino
@@ -1,36 +1,55 @@
 #include "BK8000L.h"
 #include <SoftwareSerial.h> //if using SW, with HW no need for this
 
+#include <AltSoftSerial.h>
+
 #define LED_PIN A1
 
-SoftwareSerial sfSerial(7, 6); //rxPin, txPin
+AltSoftSerial altSerial;
 
-String str;
+//AltSoftSerial rxPin = D8
+//AltSoftSerial txPin = D9
+
+String line;
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  while (!Serial);
+    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+    while (!Serial);
 
-  sfSerial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  while (!sfSerial);
-  // set the data rate for the SoftwareSerial port
-
-  pinMode(LED_PIN, OUTPUT);
+    altSerial.begin(9600);
+    
+    pinMode(LED_PIN, OUTPUT);
   
-  Serial.println("ARD:Ready");
-
-  delay(3000);
-  sfSerial.println("APT+SPP8888");
+    delay(1500);
+    altSerial.print("APT+SPP8888\r\n");
+    
+    Serial.println("ARD:Ready");
 }
 
 void loop() {
-  if (sfSerial.available() > 0) {
-    str = sfSerial.readString(); // read the incoming byte:
-    Serial.print("BK8000L: ");
-    Serial.println(str);
-    checkCmd(str);
-  }
+
+    char c;
+
+    if (Serial.available()) {
+        c = Serial.read();
+        altSerial.print(c);
+    }
+    if (altSerial.available()) {
+        c = altSerial.read();
+        //Serial.print(c);
+        
+        if(c != '\n'){
+            line += c;
+        } else {
+            // end of line
+        
+            Serial.print("BK8000L:");
+            Serial.println(line);         
+            checkCmd(line);
+            
+            line = ""; 
+        }
+    }
 }
 
 void checkCmd(String cmd){
